d3372838e49cc1da1fc3539cc0e582bd5d47fbc7
„Grzegorz <grzegorzgulba@gmail.com>
2025-11-01 12:55:47 +0100

    Initial commit

---
+++ src/main/java/pl/grzegorz/financeapp/SimpleController.java
@@ -0,0 +1,12 @@
+package pl.grzegorz.financeapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello from Spring Boot!";
+    }
+}
---
+++ src/test/java/pl/grzegorz/financeapp/FinanceappApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.grzegorz.financeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
