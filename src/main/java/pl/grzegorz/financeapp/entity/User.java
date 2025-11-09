657779c3944d361415eacc391d6188f37b35884c
„Grzegorz <grzegorzgulba@gmail.com>
2025-11-09 18:02:12 +0100

    adding UserController, and update User

--- src/main/java/pl/grzegorz/financeapp/entity/User.java
+++ src/main/java/pl/grzegorz/financeapp/entity/User.java
@@ -21,4 +21,36 @@ public class User {
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<Transaction> transactions = new ArrayList<>();
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
